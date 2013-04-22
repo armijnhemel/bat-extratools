7c925ad15b5f8b3736691362bf82c89da91f9e75
Armijn Hemel <armijn@tjaldur.nl>
2013-04-22 21:18:05 +0000

    process more data

--- bat-extratools/unyaffs/unyaffs.py
+++ bat-extratools/unyaffs/unyaffs.py
@@ -56,41 +56,32 @@ while not bytesread >= os.stat('yaffs2_1.img').st_size:
 	#print chunkId
 	byteCount = struct.unpack('<L', sparedata[12:16])[0]
 
-	'''
-typedef struct {
-    yaffs_ObjectType type;
-
-        /* The following apply to directories, files, symlinks - not hard links */
-        __u32 yst_mode;         /* protection */
-
-    __u32 yst_uid;
-    __u32 yst_gid;
-    __u32 yst_atime;
-    __u32 yst_mtime;
-    __u32 yst_ctime;
-	'''
-
 	## check if the file is a new file
 	if byteCount == 0xffff:
+		offset = 0
 		## new file, so process the chunk data
 		## first read in the object header
-		chunktype = struct.unpack('<L', chunkdata[0:4])[0]
+		chunktype = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
 
 		## unused checksum, can be ignored
-		parentObjectId = struct.unpack('<L', chunkdata[4:8])[0]
+		parentObjectId = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
 
 		## store the parent id for this object, so it can be retrieved later
 		objectparent[objectId] = parentObjectId
 
 		## unused checksum, can be ignored
-		checksum = struct.unpack('<H', chunkdata[8:10])[0]
+		checksum = struct.unpack('<H', chunkdata[offset:offset+2])[0]
+		offset = offset + 2
 
 		## extract the name, up to the first '\x00' character
 		## and store it
-		yaffsname = chunkdata[10:10+YAFFS_MAX_NAME_LENGTH+1+2]
+		yaffsname = chunkdata[offset:offset+YAFFS_MAX_NAME_LENGTH+1+2]
 		eoname = yaffsname.find('\x00')
 		yaffsname=yaffsname[:eoname]
 		objectidtoname[objectId] = yaffsname
+		offset = offset + YAFFS_MAX_NAME_LENGTH + 1 + 2
 
 		## reconstruct the full path
 		if objectId != 1 and objectidtoname.has_key(parentObjectId) and objectId != parentObjectId:
@@ -101,10 +92,46 @@ typedef struct {
 				newparentid = objectparent[newparentid]
 			print yaffsname
 
+		yst_mode = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		yst_uid = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		yst_gid = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		yst_atime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		yst_mtime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		yst_ctime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		fileSize = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+		equivalentObjectId = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+		offset = offset + 4
+
+    		#char alias[YAFFS_MAX_ALIAS_LENGTH + 1];
+		## extract the alias, up to the first '\x00' character
+		## and store it
+		aliasname = chunkdata[offset:offset+YAFFS_MAX_ALIAS_LENGTH+1]
+		eoname = aliasname.find('\x00')
+		if eoname != -1:
+			aliasname = aliasname[:eoname]
+		else:
+			aliasname = None
+		offset = offset + YAFFS_MAX_ALIAS_LENGTH + 1
+
 		if chunktype == YAFFS_OBJECT_TYPE_FILE:
 			print "CHUNK file"
 		elif chunktype == YAFFS_OBJECT_TYPE_SYMLINK:
-			print "CHUNK symlink"
+			if aliasname != None:
+				print "ALIAS", aliasname
 		elif chunktype == YAFFS_OBJECT_TYPE_DIRECTORY:
 			## create the directory and move on
 			print "CHUNK directory"
@@ -115,6 +142,9 @@ typedef struct {
 		else:
 			print "CHUNK unknown"
 		pass
+	else:
+		## block with data
+		print "BYTECOUNT", byteCount
 
 	## move on to the next chunk
 	bytesread = bytesread + len(chunkdata) + len(sparedata)
