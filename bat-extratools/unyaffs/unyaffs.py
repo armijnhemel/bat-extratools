08e9e10575c3127be1f8cb5aeb5088b19462a512
Armijn Hemel <armijn@tjaldur.nl>
2013-04-23 13:07:57 +0000

    prepare for other values than the default ones

--- bat-extratools/unyaffs/unyaffs.py
+++ bat-extratools/unyaffs/unyaffs.py
@@ -15,11 +15,9 @@ by Kai Wei
 import sys, os, os.path, struct
 from optparse import OptionParser
 
+
 ## some hardcoded settings copied from unyaffs.c and unyaffs.h
 ## In other devices these settings are actually different
-CHUNK_SIZE=2048
-SPARE_SIZE=64
-
 YAFFS_OBJECTID_ROOT=1
 YAFFS_MAX_NAME_LENGTH=255
 YAFFS_MAX_ALIAS_LENGTH=159
@@ -54,141 +52,156 @@ def main(argv):
 	else:
 		unpackdir = options.unpackdir
 
+	yaffs2filesize = os.stat(yaffs2file).st_size
+
+	#chunksandspares = [(2048,64), (1024,64)]
+	chunksandspares = [(2048,64)]
+	possibleyaffs = False
+
+	for cs in chunksandspares:
+		(CHUNK_SIZE, SPARE_SIZE) = cs
+		if yaffs2filesize%(CHUNK_SIZE + SPARE_SIZE) == 0:
+			possibleyaffs = True
+			break
+
+	if not possibleyaffs:
+		if options.verbose:
+			print >>sys.stderr, "not a valid YAFFS2 image"
+		sys.exit(1)
+
 	image_file = open(yaffs2file)
 
 	image_file.seek(0)
-	bytesread = 0
 
-	yaffs2filesize = os.stat(yaffs2file).st_size
+	yaffsunpacked = False
+	for cs in chunksandspares:
+		if yaffsunpacked:
+			break
+		(CHUNK_SIZE, SPARE_SIZE) = cs
+		bytesread = 0
+		yaffsunpackfail = False
 
-	if yaffs2filesize%(CHUNK_SIZE + SPARE_SIZE) != 0:
-		## this is not a valid yaffs2 file for this chunk size and spare size
-		if options.verbose:
-			print >>sys.stderr, "not a valid YAFFS2 image"
-		image_file.close()
-		sys.exit(1)
+		objectidtoname = {}
+		objectparent = {}
+		outfile = None
 
-	objectidtoname = {}
-	objectparent = {}
+		## read in the blocks of data
+		while not bytesread >= yaffs2filesize and not yaffsunpackfail:
+			chunkdata = image_file.read(CHUNK_SIZE)
+			sparedata = image_file.read(SPARE_SIZE)
 
-	outfile = None
+			## read in the spare data first and extract some metadata
+			sequenceNumber = struct.unpack('<L', sparedata[0:4])[0]
+			objectId = struct.unpack('<L', sparedata[4:8])[0]
+			chunkId = struct.unpack('<L', sparedata[8:12])[0]
+			byteCount = struct.unpack('<L', sparedata[12:16])[0]
 
-	## read in the blocks of data
-	while not bytesread >= yaffs2filesize:
-		chunkdata = image_file.read(CHUNK_SIZE)
-		sparedata = image_file.read(SPARE_SIZE)
+			## check if the file is a new file
+			if byteCount == 0xffff:
+				if outfile != None:
+					outfile.close()
+				offset = 0
+				oid = objectId
+				## new file, so process the chunk data
+				## first read in the object header
+				chunktype = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-		## read in the spare data first and extract some metadata
-		sequenceNumber = struct.unpack('<L', sparedata[0:4])[0]
-		objectId = struct.unpack('<L', sparedata[4:8])[0]
-		chunkId = struct.unpack('<L', sparedata[8:12])[0]
-		byteCount = struct.unpack('<L', sparedata[12:16])[0]
+				## unused checksum, can be ignored
+				parentObjectId = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-		## check if the file is a new file
-		if byteCount == 0xffff:
-			if outfile != None:
-				outfile.close()
-			offset = 0
-			oid = objectId
-			## new file, so process the chunk data
-			## first read in the object header
-			chunktype = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				## store the parent id for this object, so it can be retrieved later
+				objectparent[objectId] = parentObjectId
 
-			## unused checksum, can be ignored
-			parentObjectId = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				## unused checksum, can be ignored
+				checksum = struct.unpack('<H', chunkdata[offset:offset+2])[0]
+				offset = offset + 2
 
-			## store the parent id for this object, so it can be retrieved later
-			objectparent[objectId] = parentObjectId
+				## extract the name, up to the first '\x00' character
+				## and store it
+				yaffsname = chunkdata[offset:offset+YAFFS_MAX_NAME_LENGTH+1+2]
+				eoname = yaffsname.find('\x00')
+				yaffsname=yaffsname[:eoname]
+				objectidtoname[objectId] = yaffsname
+				offset = offset + YAFFS_MAX_NAME_LENGTH + 1 + 2
 
-			## unused checksum, can be ignored
-			checksum = struct.unpack('<H', chunkdata[offset:offset+2])[0]
-			offset = offset + 2
+				## reconstruct the full path
+				if objectId != 1 and objectidtoname.has_key(parentObjectId) and objectId != parentObjectId:
+					objectpath = yaffsname
+					newparentid = parentObjectId
+					while newparentid != 1:
+						yaffsname = os.path.join(objectidtoname[newparentid], yaffsname)
+						newparentid = objectparent[newparentid]
+					if options.verbose:
+						print "unpacking", yaffsname
 
-			## extract the name, up to the first '\x00' character
-			## and store it
-			yaffsname = chunkdata[offset:offset+YAFFS_MAX_NAME_LENGTH+1+2]
-			eoname = yaffsname.find('\x00')
-			yaffsname=yaffsname[:eoname]
-			objectidtoname[objectId] = yaffsname
-			offset = offset + YAFFS_MAX_NAME_LENGTH + 1 + 2
+				yst_mode = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			## reconstruct the full path
-			if objectId != 1 and objectidtoname.has_key(parentObjectId) and objectId != parentObjectId:
-				objectpath = yaffsname
-				newparentid = parentObjectId
-				while newparentid != 1:
-					yaffsname = os.path.join(objectidtoname[newparentid], yaffsname)
-					newparentid = objectparent[newparentid]
-				if options.verbose:
-					print "unpacking", yaffsname
+				yst_uid = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			yst_mode = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				yst_gid = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			yst_uid = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				yst_atime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			yst_gid = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				yst_mtime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			yst_atime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				yst_ctime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			yst_mtime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				fileSize = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			yst_ctime = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				equivalentObjectId = struct.unpack('<L', chunkdata[offset:offset+4])[0]
+				offset = offset + 4
 
-			fileSize = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
+				## extract the alias, up to the first '\x00' character
+				## and store it
+				aliasname = chunkdata[offset:offset+YAFFS_MAX_ALIAS_LENGTH+1]
+				eoname = aliasname.find('\x00')
+				if eoname != -1:
+					aliasname = aliasname[:eoname]
+				else:
+					aliasname = None
+				offset = offset + YAFFS_MAX_ALIAS_LENGTH + 1
 
-			equivalentObjectId = struct.unpack('<L', chunkdata[offset:offset+4])[0]
-			offset = offset + 4
-
-			## extract the alias, up to the first '\x00' character
-			## and store it
-			aliasname = chunkdata[offset:offset+YAFFS_MAX_ALIAS_LENGTH+1]
-			eoname = aliasname.find('\x00')
-			if eoname != -1:
-				aliasname = aliasname[:eoname]
+				if chunktype == YAFFS_OBJECT_TYPE_FILE:
+					outname = os.path.join(unpackdir, yaffsname)
+					outfile = open(outname, 'wb')
+				elif chunktype == YAFFS_OBJECT_TYPE_SYMLINK:
+					if aliasname != None:
+						symlink = os.path.join(unpackdir, yaffsname)
+						os.symlink(aliasname, symlink)
+				elif chunktype == YAFFS_OBJECT_TYPE_DIRECTORY:
+					## create the directory and move on
+					createdir = os.path.join(unpackdir, yaffsname)
+					try:
+						os.makedirs(createdir)
+					except:
+						pass
+					yaffsunpacked = True
+				elif chunktype == YAFFS_OBJECT_TYPE_HARDLINK:
+					print "CHUNK hardlink"
+				elif chunktype == YAFFS_OBJECT_TYPE_SPECIAL:
+					print "CHUNK special"
+				else:
+					print "CHUNK unknown"
+					yaffsunpackfail = True
 			else:
-				aliasname = None
-			offset = offset + YAFFS_MAX_ALIAS_LENGTH + 1
+				## block with data, just write the chunkdata to the output file
+				if outfile != None:
+					outfile.write(chunkdata[:byteCount])
 
-			if chunktype == YAFFS_OBJECT_TYPE_FILE:
-				outname = os.path.join(unpackdir, yaffsname)
-				outfile = open(outname, 'wb')
-			elif chunktype == YAFFS_OBJECT_TYPE_SYMLINK:
-				if aliasname != None:
-					symlink = os.path.join(unpackdir, yaffsname)
-					os.symlink(aliasname, symlink)
-			elif chunktype == YAFFS_OBJECT_TYPE_DIRECTORY:
-				## create the directory and move on
-				createdir = os.path.join(unpackdir, yaffsname)
-				try:
-					os.makedirs(createdir)
-				except:
-					pass
-			elif chunktype == YAFFS_OBJECT_TYPE_HARDLINK:
-				print "CHUNK hardlink"
-			elif chunktype == YAFFS_OBJECT_TYPE_SPECIAL:
-				print "CHUNK special"
-			else:
-				print "CHUNK unknown"
-			pass
-		else:
-			## block with data, just write the chunkdata to the output file
-			if outfile != None:
-				outfile.write(chunkdata[:byteCount])
+			## move on to the next chunk
+			bytesread = bytesread + len(chunkdata) + len(sparedata)
 
-		## move on to the next chunk
-		bytesread = bytesread + len(chunkdata) + len(sparedata)
-
-	if outfile != None:
-		outfile.close()
+		if outfile != None:
+			outfile.close()
 	image_file.close()
 
 if __name__ == "__main__":
